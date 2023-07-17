4a9f9157f9b3681b3226413623476d386e253386
Enoch Benjamin <enoch.benjamin@biourja.com>
2023-07-17 17:00:42 +0530

    edited trade date area

--- process_trident.py
+++ process_trident.py
@@ -63,7 +63,9 @@ def get_trade_date():
         file_name= os.listdir(os.getcwd() + "\\Download")
         file2=file_name[0]
         # test_area_date = ["67","47","85","160"]
-        test_area_date=["70.763","43.605","83.003","145.35"]
+        # test_area_date=["56.993","-0.704","93.713","332.836"]
+        # test_area_date=["70.763","43.605","83.003","145.35"]
+        test_area_date=["8.798","1.591","272.723","172.186"]
         df_date = tabula.read_pdf(download_path + '\\' + file2,lattice=True,stream=True, multiple_tables=True,pages="1",area=test_area_date,silent=True,guess=False)
         trade_date=df_date[0].columns[1]
         return trade_date
